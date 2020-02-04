656000719c8adbec36089bda0a1314b243ee5f1d
kmbwollam <wollamk@oregonstate.edu>
2020-02-03 16:03:39 -0800

    Update parser.cpp
    parser updated with main for independant testing.  NOT CONNECTED TO MAIN or GAME OBJECT, recognizes basic commands (look, help, inventory, savegame, loadgame) and cardinal directions (north, south, east, west) as well as directions preceeded by go, to, the.

--- parser.cpp
+++ parser.cpp
@@ -2,13 +2,41 @@
 #include <vector> 
 #include <string>
 #include <sstream>
+#include <iterator>
+
+using namespace std;
 
 int inputParsing(string userInput);
 string cleanInput(string userInput); 
 int checkBasicCommands(string cleanInput); 
-vector splitCleanInput(string cleanInput);
-int checkMoveCommands(vector inputVector); 
-int checkActions(vector input Vector); 
+vector<string> splitCleanInput(string cleanInput);
+int checkMoveCommands(vector<string> inputVector); 
+int checkActions(vector<string> inputVector); 
+
+int main() {
+	string input = "help"; 
+	int status = inputParsing(input);
+	input = "look"; 
+	status = inputParsing(input); 
+	input = "inventory";
+	status = inputParsing(input);
+	input = "savegame";
+	status = inputParsing(input);
+	input = "loadgame";
+	status = inputParsing(input);
+	input = "one two three four five";
+	status = inputParsing(input);
+	input = "north";
+	status = inputParsing(input);
+	input = "go east";
+	status = inputParsing(input);
+	input = "go to south";
+	status = inputParsing(input);
+	input = "go to the west";
+	status = inputParsing(input);
+
+	return 0;
+}
 
 
 //main parsing function that will be called from the main game
@@ -16,17 +44,17 @@ int checkActions(vector input Vector);
 //calls any necessary commands
 //returns 0 if command recognized and executed
 //prints error and returns 1 if invalid command
-inputParsing(userInput){
-	string cleanInputs = cleanInputs(userInput); 
+int inputParsing(string userInput){
+	string clean = cleanInput(userInput); 
 	int status = 0; 
-	status = checkBasicCommands(cleanInput);
+	status = checkBasicCommands(clean);
 	if (status == 1) return 0; 
-	vector wordList = splitCleanInput(cleanInput); 
+	vector<string> wordList = splitCleanInput(clean); 
 	status = checkMoveCommands(wordList); 
 	if (status == 1) return 0; 
-	status = int checkActions(wordList); 
+	status = checkActions(wordList); 
 	if (status == 1) return 0;
-	else (give error statement);
+	else //(give error statement);
 	return 1;
 }
 
@@ -36,7 +64,7 @@ inputParsing(userInput){
 //returns clean string back
 //NOT IMPLEMENTED
 
-cleanInput(userInput){
+string cleanInput(string userInput){
 	return userInput;
 }
 
@@ -45,21 +73,26 @@ cleanInput(userInput){
 //calls the corresponding functions
 //Returns an int 0 if no basic command is found or 1 if command found and executed
 //needs current room, player, and game
-checkBasicCommands(cleanInput){
+int checkBasicCommands(string cleanInput){
 	int basicStatus =1;
 	if (cleanInput == "look"){
+		cout << "look called\n";
 		//call current room long description
 	}
 	else if (cleanInput == "help"){
+		cout << "help called \n";
 		//call help function
 	}
 	else if (cleanInput == "inventory"){
+		cout << "inventory called \n"; 
 		//call display player inventory
 	}
 	else if (cleanInput == "savegame"){
+		cout << "savegame called \n";
 		//call savegame
 	}
 	else if (cleanInput == "loadgame"){
+		cout << "loadgame called \n"; 
 		//call load game
 	}
 	else {
@@ -72,12 +105,13 @@ checkBasicCommands(cleanInput){
 //words are stored in a vector for ease of parsing
 //returns wordListvector
 //reference: https://stackoverflow.com/questions/5607589/right-way-to-split-an-stdstring-into-a-vectorstring
-splitCleanInput(cleanInput){
+vector<string> splitCleanInput(string cleanInput){
 	stringstream stream(cleanInput);
 	istream_iterator<string> begin(stream);
 	istream_iterator<string> end;
 	vector<string> wordlist(begin, end);
-	copy(wordlist.begin(), wordlist.end();
+	copy(wordlist.begin(), wordlist.end(), ostream_iterator<string>(cout, "\t"));
+	cout << "\n";
 	return wordlist;
 }
 
@@ -85,10 +119,31 @@ splitCleanInput(cleanInput){
 //recognizes: go <direction/room>, <direction/room>
 //calls the correct movement function
 //returns an int 0 if no movement command is found or 1 if command found and executed
-//NOT IMPLEMENTED
-checkMoveCommands(inputVector){
-	int moveStatus = 0;
-	
+//directions IMPLEMENTD, room names NOT IMPLEMENTED
+int checkMoveCommands(vector<string> inputVector){
+	int moveStatus = 1;
+	while (inputVector[0] == "go" || inputVector[0] == "to" || inputVector[0] == "the") {
+		inputVector.erase(inputVector.begin());
+	}
+	if (inputVector[0] == "north") {
+		cout << "called move north\n";
+		//add movement to north
+	}
+	else if (inputVector[0] == "east") {
+		cout << "called move east\n";
+		//add movement to north
+	}
+	else if (inputVector[0] == "south") {
+		cout << "called move south\n";
+		//add movement to north
+	}
+	else if (inputVector[0] == "west") {
+		cout << "called move west\n";
+		//add movement to north
+	}
+	else {
+		moveStatus = 0;
+	}
 	return moveStatus;
 }
 
@@ -96,7 +151,7 @@ checkMoveCommands(inputVector){
 //If a pair is found calls the correct function
 //Returns an int 0 if no action command is found or 1 if command found and executed
 //NOT IMPLEMENTED
-int checkActions(vector inputVector){
+int checkActions(vector<string> inputVector){
 	int actionStatus = 0;
 	
 	return actionStatus;
