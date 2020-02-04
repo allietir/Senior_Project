8bf7d1eabeb2193e24870a5ec1d0697cb936e47a
Alex Tir <tira@oregonstate.edu>
2020-02-03 16:41:00 -0800

    Fix minor typo

--- Feature/Feature1.cpp
+++ Feature/Feature1.cpp
@@ -22,7 +22,7 @@ Feature1::Feature1() {
 }
 
 void Feature1::read(){
-	string read_response = "The " + get_name() + "'s first name is too faded to read, but the last name says 'Smith'.\n 'Hey', says" + FRIEND_NAME + ". 'Wasn't that your mom's maiden name?'. 'Yeah', you reply, ";
+	string read_response = "The " + get_name() + "'s first name is too faded to read, but the last name says 'Smith'.\n 'Hey', says " + FRIEND_NAME + ". 'Wasn't that your mom's maiden name?'. 'Yeah', you reply, ";
 	func_togg_count_x(0);
 	
 	printf("%s\n", read_response.c_str());	
