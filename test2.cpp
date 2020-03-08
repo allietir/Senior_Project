b3c8e15b6dd75a2dfeb275fe9c7dddb4724fd0fb
Claudia Rodriguez-Schroeder <claudiarodriguez-schroeder@claudias-mbp.home>
2020-03-07 16:22:56 -0800

    adsf

--- test2.cpp
+++ test2.cpp
@@ -15,15 +15,20 @@ int main()
 {
 		char buffer[1024];
 		FILE *fp = fmemopen(buffer, 1024, "w");
-		if ( !fp ) { printf("error"); return 0; }
-
+		printf("reached this\n");
+		if ( !fp ) { printf("the error"); return 0; }
+		printf("failed here 1;");
 		FILE *old = stdout;
+		printf("failed here 2;");
 		stdout = fp;
-
+		printf("failed here 3;");
+		
 		foo(); //all the printf goes to buffer (using fp);
-
+		printf("failed here 4;");
 		fclose(fp);
+		printf("failed here 5;");
 		stdout = old; //reset
-
+		printf("failed here 6;");
 		printf("<redirected-output>\n%s</redirected-output>", buffer);
+		printf("failed here 7;");
 }
