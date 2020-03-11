9a23761269d774e119b020eca19486fdd90abb89
Claudia Rodriguez-Schroeder <claudiarodriguez-schroeder@claudias-mbp.home>
2020-03-10 18:58:51 -0700

    look returns long form of room per reqs.

--- Game.cpp
+++ Game.cpp
@@ -294,7 +294,7 @@ void Game::exit_room(int dir){
 
 }
 void Game::look(){
-	r_array[player1.get_current_room()]->look();
+	printf("%s", r_array[player1.get_current_room()]->get_long_description().c_str());
 }
 void Game::help(){
 	for (int i = 0; i < NUM_VERB_FUNCS; i++){
