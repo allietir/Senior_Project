4da6af3655a4eec78f456461f5775375e173cccc
Claudia Rodriguez-Schroeder <claudiarodriguez-schroeder@claudias-mbp.home>
2020-03-11 17:32:33 -0700

    removed color text for features not in room in portrati feature (feature3.cpp) per bug request

--- Feature/Feature3.cpp
+++ Feature/Feature3.cpp
@@ -6,8 +6,8 @@
 
 Feature3::Feature3() {
 	set_name("\033[1;31mPortrait\033[0m");
-	desc1 = "There is a \033[1;31mportrait\033[0m of a stern looking family on the wall. A \033[1;35mlamp\033[0m appears below it. Their features are blurred but they are dressed in old fashioned clothing and are unsmiling. There's a tall, dark handsome man with a blood-thirsty look in his eyes. There's a meek looking \033[1;31mwoman\033[0m who seems to be staring, not at the artist, but at her hands. And there is a little \033[1;31mgirl\033[0m clutching what looks like a \033[1;35mdoll\033[0m, but you can't see much more. It looks like a corner of the \033[1;31mportrait\033[0m is peeling. What could be underneath? Can you try to open it? ";
-	string desc2 = "There is a \033[1;31mportrait\033[0m of a stern looking family on the wall. Their features are blurred but they are dressed in old fashioned clothing and are unsmiling. There's a tall, dark handsome man with a bloodthirsty look in his eyes. There's a meek looking \033[1;31mwoman\033[0m who seems to be staring, not at the artist, but at her hands. And there is a little \033[1;31mgirl\033[0m clutching what looks like a \033[1;35mdoll\033[0m, but you can't see much more. It looks like a corner of the \033[1;31mportrait\033[0m is peeling. What could be underneath? Can you try to open it? ";
+	desc1 = "There is a \033[1;31mportrait\033[0m of a stern looking family on the wall. A \033[1;35mlamp\033[0m appears below it. Their features are blurred but they are dressed in old fashioned clothing and are unsmiling. There's a tall, dark handsome man with a blood-thirsty look in his eyes. There's a meek looking woman who seems to be staring, not at the artist, but at her hands. And there is a little girl clutching what looks like a doll but you can't see much more. It looks like a corner of the \033[1;31mportrait\033[0m is peeling. What could be underneath? Can you try to open it? ";
+	string desc2 = "There is a \033[1;31mportrait\033[0m of a stern looking family on the wall. Their features are blurred but they are dressed in old fashioned clothing and are unsmiling. There's a tall, dark handsome man with a bloodthirsty look in his eyes. There's a meek looking woman who seems to be staring, not at the artist, but at her hands. And there is a little girl clutching what looks like a doll, but you can't see much more. It looks like a corner of the \033[1;31mportrait\033[0m is peeling. What could be underneath? Can you try to open it? ";
 	set_desc(desc1);
 	set_desc_no_obj(desc2);
 	
@@ -31,12 +31,12 @@ int Feature3::use(int feat_obj_id){
 	
 	if (feat_obj_id == 0){
 		if(get_times_toggled(2)==0){
-				string use_event = "You feel a chill and you see the \033[1;35mdoll\033[0m in the \033[1;31mgirl's\033[0m arms disapear. The \033[1;31mgirl's\033[1;31m face grows even sadder, and you see a single tear drop out of the \033[1;31mportrait\033[0m and onto your feet. ";
+				string use_event = "You feel a chill and you see the doll in the girl arms disapear. The girl face grows even sadder, and you see a single tear drop out of the \033[1;31mportrait\033[0m and onto your feet. ";
 					func_togg_count_x(2);
 					printf("%s\n", use_event.c_str());
 			}
 			if(get_times_toggled(2)>1){
-				string use_event = "Poor little \033[1;31mgirl\033[0m. You are creeped out but sad for the family in the \033[1;31mportrait\033[0m. ";
+				string use_event = "Poor little girl. You are creeped out but sad for the family in the \033[1;31mportrait\033[0m. ";
 				func_togg_count_x(2);
 				printf("%s\n", use_event.c_str());
 
